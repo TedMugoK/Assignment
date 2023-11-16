9caf353a34eab7a7582c2a84babd88e37f132a42
TedMugoK <145543848+TedMugoK@users.noreply.github.com>
2023-11-16 09:25:31 +0300

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,13 @@
+public class StudentDetails{
+public static void main(String args[]){
+
+System.out.println("Instution:KCA University");
+System.out.println("Course:DIT");
+System.out.println("Name:Ted Mugo Kiiru");
+System.out.println("Reg no:22/03500");
+System.out.println("Gender:Male");
+System.out.println("DOB:2/8/1980");
+System.out.println("Phone no:0727077094");
+System.out.println("Email Address:mugotedk@gmail.com");
+}
+}
